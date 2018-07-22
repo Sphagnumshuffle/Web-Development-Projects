098de1c1b1cf0fad6f4d6ebd81579b8347e21df3
Ville Mäkinen <31400393+Sphagnumshuffle@users.noreply.github.com>
2018-07-22 14:17:46 +0300

    Media Queries and navbar adjustments

--- Portfolio Page/main.js
+++ Portfolio Page/main.js
@@ -1,11 +1,11 @@
 // Animate Smooth Scroll
 
 $('#view-work').on('click', function() {
-    const images = $('images').position().top;
+    const projects = $('#projects').position().top;
 
     $('html, body').animate (
         {
-            scrollTop: images
+            scrollTop: projects
         },
         900 //Time in milliseconds
     )
